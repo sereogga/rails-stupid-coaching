a7ca1addeb33c206bbf012118623f51ab758cb87
sergio bodean <sereoga24@hotmail.com>
2023-05-22 15:00:08 +0100

    added bootstrap style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,8 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    if params["question"]
-      @answer = params["question"]
+    @question = params["question"]
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.last == "?"
+       @answer = "Silly question, get dressed and go to work!"
+    else
+       @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
